105ea3fec4ec11147aa70342656713df47c9059e
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2022-02-02 19:29:06 -0300

    ID-#1 Spring MVC PoC V1 - HelloMVC and thymeleaf

---
+++ coffee-store-front-v1/store-v1/src/test/java/io/github/pabloin/storev1/StoreV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pabloin.storev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
